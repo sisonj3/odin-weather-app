4f57a06a969f9435400c2125ad6cef31de62b2e3
sisonj <98570174+sisonj3@users.noreply.github.com>
2023-10-23 15:47:55 -0700

    Added function to get weather data of inputted location

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import "./style.css";
+import getWeatherData from './weather-data';
 
 console.log("Starting Program...");
+
+getWeatherData('london');
---
+++ src/weather-data.js
@@ -0,0 +1,11 @@
+
+async function getWeatherData(location) {
+    const url = `https://api.weatherapi.com/v1/current.json?key=c06666a5ce6c406e9d3211443232010&q=${location}`;
+
+    const response = await fetch(url, {mode: 'cors'});
+    const weatherData = await response.json();
+
+    console.log(weatherData);
+}
+
+export default getWeatherData;
